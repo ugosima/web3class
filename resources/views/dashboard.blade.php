f65ae20e102136d6af0d598a86b078c66f815b43
ugosima <84172561+ugosima@users.noreply.github.com>
2026-06-02 07:38:58 +0100

    Testing ads block with website

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -381,6 +381,12 @@
                 
 
                 <div class="border-t border-slate-200 dark:border-slate-800 dark:bg-gray-300 my-8 "></div>
+                {{-- Ads player --}}
+                
+                 <div id="adsbox" class="rounded-lg border border-white/10 bg-slate-950/60 p-4 mb-8">
+
+                 </div>
+                 <script type="text/javascript" src="https://cdn.applixir.com/applixir.app.v6.1.0.js"></script>
         
                 <h2 class="text-2xl font-bold text-gray-200 dark:text-red-500  mb-4">PRACTICE QUESTIONS</h2>
                 <p class="text-slate-400 dark:text-slate-700 font-medium mb-4">Answer all questions correctly to progress to the next lesson.</p>
@@ -590,6 +596,8 @@
                            } else {
                             showPopup('welcomePopup', `You answered ${data.score} out of 6 questions correctly! Watch ${6 - data.score} ads to continue!`);
                             btn.innerHTML = 'Watch Ads';
+                            btn.id = 'watchAdsButton';
+
                         }
                     } else if (xhr.status === 419) {
                         let message = 'Request expired: please reload and try again';
@@ -638,7 +646,39 @@
 });
 
 
+////////Ads codeblock
+
+
+
+
+                            const options = {
+                            apiKey: "44e9c330-c5d0-4dae-8a76-dbe3982879a5", // Replace with your actual API key
+                            injectionElementId: "adsbox", // This is the ID of the div from step 2.
+                            adStatusCallbackFn: (status) => { // This is how you can listen for ad statuses (more in Step 4)
+                            console.log("OUTSIDE Ad status: ", status);
+                            },
+                            adErrorCallbackFn: (error) => { // This is how you can listen for errors (more in Step 4)
+                            console.log("Error: ", error.getError().data);
+                            },
+                        };
+
+                        var application = new Application(options);
+                        window.onload = () => { // Important: Initialize on window load!!! (or use timeout)
+                            application.initialize();
+                            
+                        };
+
+
+
        
     </script>
+
+
+
+
+
+
+
+
         @vite('resources/js/toggledark.js')
 </x-app-layout>
